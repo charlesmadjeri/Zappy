0192adc4501bbf5c3911b0590ae9949373795302
Maksen <maksen.mofredj@gmail.com>
2024-06-17 12:17:16 +0200

    [ADD] in ai connection,  basic connection implementation (can connect to any zappy_server,get assigned to a team and get the size of the world

--- AI/main.py
+++ AI/main.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 from AI import connect
-from AI import search
-from AI.search import Player
 import sys
 import socket
 
 def main():
-    if len(sys.argv) != 5 and len(sys.argv) != 7:
+    if len(sys.argv) != 5 and len != 7:
         print("Incorrect number of argument")
         sys.exit(84)
 
@@ -23,13 +21,15 @@ def main():
             machine_name = sys.argv[i + 1]
     
     if server_port is None or team_name is None:
-        print("Missing arguments\n")
+        print("Missing arguments")
         sys.exit(84)
 
-    player = Player()
-    map_size = connect.innit_connection(player.client, machine_name, server_port, (team_name + '\n'))
-    search.spiral_search(player)
-    player.client.close()
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    map_size = connect.innit_connection(client, machine_name, server_port, (team_name + '\n'))
+    i = 0
+    while i >= 0:
+        i += 1
+    client.close()
     
 
 if __name__ == "__main__":
