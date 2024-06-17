53a9f0ae3191f3a3a7f2f041345663813a93fa83
Maksen <maksen.mofredj@gmail.com>
2024-06-17 12:17:16 +0200

    [ADD] in ai connection,  basic connection implementation (can connect to any zappy_server,get assigned to a team and get the size of the world

---
+++ AI/connect.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import socket
+import sys
+
+def innit_connection(client, machine_name, server_port, team_name):
+    try:
+        client.connect((machine_name, server_port))
+        print(f"Connected to {machine_name}:{server_port}")
+
+        initial_data = client.recv(10)
+        print(f"Received from server: {initial_data.decode()}")
+
+        client.sendall(team_name.encode())
+
+        data = client.recv(60).decode()
+        print(f"Received from server: {data}")
+
+    except socket.error as e:
+        print(f"Connection error: {e}")
+    
+    split_data = data.split('\n')
+    client_number = int(split_data[0])
+
+    if client_number < 1 :
+        client.close()
+        print(f"Not enough space in team: {team_name}")
+        sys.exit(84)
+
+    map_size = tuple(map(int, split_data[1].split()))
+
+    return map_size
