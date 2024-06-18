746ba0f8e6c45e2653856a0aa32073d1f160e9ff
Sofiane Larbi <sofiane.larbi@epitech.eu>
2024-06-18 13:47:52 +0200

    [WIP]: Initiate signal and socket creation

--- server/include/server.h
+++ server/include/server.h
@@ -5,12 +5,16 @@
 ** server
 */
 
+#pragma once
+
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+typedef struct sockaddr_in sockaddr_in_t;
 
 /*typedef enum ressource_s {
     FOOD,
@@ -59,7 +63,7 @@ typedef struct player_s {
 /*client_struct*/
 typedef struct client_s {
     int fd;
-    sockaddr_in address;
+    sockaddr_in_t address;
     player_t *player;
 } client_t;
 
@@ -70,7 +74,12 @@ typedef struct linked_client_s {
 } linked_lient_t;
 
 typedef struct server_s {
-
+    int port;
+    int sockfd;
+    sockaddr_in_t addr_serv;
+    socklen_t socket_size;
+    client_t *sever_client;
+    linked_lient_t clients;
 } server_t;
 
 int main_loop () {}
