1ad3e56f032b4037289303c8e93adbebcdc3c764
Sofiane Larbi <sofiane.larbi@epitech.eu>
2024-06-25 14:08:23 +0200

    [FIX]: IP settings when generating socket address

--- server/src/socket.c
+++ server/src/socket.c
@@ -12,8 +12,8 @@
 struct sockaddr_in generate_addr(const int port)
 {
     struct sockaddr_in addr;
-
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    
+    addr.sin_addr.s_addr = INADDR_ANY;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
     return addr;
